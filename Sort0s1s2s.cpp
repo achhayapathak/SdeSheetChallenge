a33dcd4cd1cebbad256317102ec7e1ed96018fbf
Achhaya Pathak <93811742+achhayapathak@users.noreply.github.com>
2023-06-08 22:18:11 +0530

    Update Sort0s1s2s.cpp

--- Sort0s1s2s.cpp
+++ Sort0s1s2s.cpp
@@ -16,3 +16,28 @@ void sort012(int *nums, int n)
             }
         }
 }
+
+
+
+#include <bits/stdc++.h> 
+void sort012(int *nums, int n)
+{
+
+      int zero = 0, one = 0, two = 0, i = 0;
+
+        for(int nu=0; nu<n; nu++) {
+            int num = nums[nu];
+            if(num == 0)    zero++;
+            else if(num == 1)   one++;
+            else two++;
+        }
+
+        while(i<zero) 
+            nums[i++] = 0;
+
+        while(one--)
+            nums[i++] = 1;
+
+        while(two--)
+            nums[i++] = 2;
+}
